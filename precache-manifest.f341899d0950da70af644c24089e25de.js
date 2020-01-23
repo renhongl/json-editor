2eb05d96269f8d8c85c7719f4b5e5ec36ac36236
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-23 09:16:50 +0000

    Deploy renhongl/json-editor to github.com/renhongl/json-editor.git:gh-pages

---
+++ precache-manifest.f341899d0950da70af644c24089e25de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b68f74f106aa58041aa",
+    "url": "/json-editor/static/css/main.6ad43322.chunk.css"
+  },
+  {
+    "revision": "2b68f74f106aa58041aa",
+    "url": "/json-editor/static/js/main.2b68f74f.chunk.js"
+  },
+  {
+    "revision": "3b7415ea801eebcdadbc",
+    "url": "/json-editor/static/css/1.e77eef9d.chunk.css"
+  },
+  {
+    "revision": "3b7415ea801eebcdadbc",
+    "url": "/json-editor/static/js/1.3b7415ea.chunk.js"
+  },
+  {
+    "revision": "c1c79217145115793e1d",
+    "url": "/json-editor/static/js/runtime~main.c1c79217.js"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/json-editor/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  },
+  {
+    "revision": "e952eb1b80c79344b278f40d0cd43775",
+    "url": "/json-editor/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/json-editor/precache-manifest.f341899d0950da70af644c24089e25de.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/json-editor/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
